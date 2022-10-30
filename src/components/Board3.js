807f4bf3cf0fcc514c6f06ef587a70fa4e9f2b1c
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-10-30 05:48:36 -0300

    deleting consoles

--- src/components/Board3.js
+++ src/components/Board3.js
@@ -18,14 +18,8 @@ class Board extends React.Component{
     constructor(){
         super()
         this.state={
-            console1:"",
-            console2:"",
-            console3:"",
-            console4:"",
-            console5:"",
             boardOrientation:"white",
             layout:layout.white,
-//            game:new Chess("rnb1k2r/p1P1bppp/5n2/1p6/3N4/1qN1B3/PPP2PPP/R2QKB1R w KQkq - 1 10"),
             game:new Chess(),
             selectedSquare:null,
             selectedPiece:null
@@ -53,43 +47,38 @@ class Board extends React.Component{
     }
     
     // --------------------------------------- SAN MAP
-    sanConverter(to){  // personalized hashmap for the standard algebraic notation
+    NotationToSAN(){
+        // returns a hashmap {SN:SNA} for all current possible moves, except for superpositions
+
         let moves = this.state.game.moves()
         let formattedMoves = {}  
+        let turn = this.state.game.turn()
         let __l=[] 
         moves.forEach(move=>{
+            // Convert Simplified Notation ("Ne7") to Standard Algebraic Notation (Ngxe7#)        
             let _m = move
             _m = _m
+            // remove useless notation
             .replace('x','')
             .replace('+','')
-            .replace('#','')
-            
+            .replace('#','')            
             // Pawns
-            if (_m[0]===_m[0].toLowerCase() && _m.length===3){
-                _m='P'+_m.slice(1,3)
-            }
-            if(_m.length===2){
-                _m='P'+_m
-            }
-    
-            // Castle
-            let turn = this.state.game.turn()
+            if (_m[0]===_m[0].toLowerCase() && _m.length===3){_m='P'+_m.slice(1,3)}
+            if(_m.length===2){_m='P'+_m}    
+            // check for castling
             if (_m==='O-O' && turn==='w'){_m = 'Kg1'}
             if (_m==='O-O' && turn==='b'){_m = 'Kg8'}
             if (_m==='O-O-O' && turn==='w'){_m = 'Kc1'}
             if (_m==='O-O-O' && turn==='b'){_m = 'Kc8'}
-            
-            // Superpositions
-                //  ->   this problem must be solved at handleMoverClick
-
             // check for promotions
             if (_m.includes("=")){
                 let _l = _m.length
                 _m = "P"+_m.slice(_l-4,_l-2)
             }
+            // Superpositions -> handleMoverClick
+
             // Push string
-            if(to==="toSan"){formattedMoves[_m] = move}
-            if(to==="toNotation"){formattedMoves[move] = _m}
+            formattedMoves[_m] = move
             __l.push(move+": "+_m)
                                                                             this.setState({console3:"converter: "+__l.join(' | ')})
         })
@@ -112,29 +101,32 @@ class Board extends React.Component{
         const moves = this.state.game.moves({square:_sqr})
                                                                             this.setState({console1:"global moves: "+this.state.game.moves().join(" | ")})
                                                                             this.setState({console2:"piece moves: "+this.state.game.moves({square:_sqr}).join(" | ")})
-        // Hilight available moves
-        moves.forEach(move=>{
-            
-            //  ->  extract square string & update components
+        // Add movers
+        moves.forEach(move=>{            
+            // Extract square ("e7") for all possible formats of moves in Standard Algebraic Notation ("Ngxe7#")
+            // ex.: "Ngxe7#" --> "e7"
+
+            let _l
+            let turn = this.state.game.turn()
             let _m = move
             // remove useless notation
             .replace('x','')
             .replace('+','')
             .replace('#','')
             // check for castling
-            let turn = this.state.game.turn()
             if (_m==='O-O' && turn==='w'){_m = 'Kg1'}
             if (_m==='O-O' && turn==='b'){_m = 'Kg8'}
             if (_m==='O-O-O' && turn==='w'){_m = 'Kc1'}
             if (_m==='O-O-O' && turn==='b'){_m = 'Kc8'}
             // check for promotions
-            let _l = _m.length
             if (_m.includes("=")){
+                _l = _m.length
                 _m = _m.slice(_l-4,_l-2)
             } else {
                 _l = _m.length
                 _m=_m.slice(_l-2,_l)
             }
+            //      -> Add mover
             this.reff[_m].current.addMover()
         })
         
@@ -145,7 +137,7 @@ class Board extends React.Component{
 
         // extract SAN syntax from chess.moves given an input in simplified notation
         // ex.: Nd4 -> N3xd4+
-        const NotationToSAN = this.sanConverter("toSan")
+        const NotationToSAN = this.NotationToSAN()
         let _san = NotationToSAN[
             this.state.selectedPiece.type.toUpperCase()
             +_sqr]
@@ -233,16 +225,15 @@ class Board extends React.Component{
             <div>
             <button onClick={this.flipBoard}>Flip Board</button>
             <button onClick={this.movePiece}>Move piece</button>
-            <p className="console"><strong>---</strong><br></br>{this.state.console1}</p>
-            <p className="console"><strong>---</strong><br></br>{this.state.console2}</p>
-            <p className="console"><strong>---</strong><br></br>{this.state.console3}</p>
-            <p className="console"><strong>---</strong><br></br>{this.state.console4}</p>
-            <p className="console"><strong>---</strong><br></br>{this.state.console5}</p>
-            <br /><p><strong>Selected Square: </strong>{this.state.selectedSquare}</p>
-            <br /><p><strong>Selected Piece: </strong>{this.state.selectedPiece===null?"null":this.state.selectedPiece.type}</p>
-            <br /><strong>Board:</strong>
-            <p><strong>fen: </strong>{this.state.game.fen()}</p>
-            <p><strong>msg: </strong>{this.state.msg}</p>
+            <p className="console">...<br/>{this.state.console1}</p>
+            <p className="console">...<br/>{this.state.console2}</p>
+            <p className="console">...<br/>{this.state.console3}</p>
+            <p className="console">...<br/>{this.state.console4}</p>
+            <p className="console">...<br/>{this.state.console5}</p>
+            <br /><p className="console"><strong>Selected Square: </strong>{this.state.selectedSquare}</p>
+            <br /><p className="console"><strong>Selected Piece: </strong>{this.state.selectedPiece===null?"null":this.state.selectedPiece.type}</p>
+            <p className="console"><strong>fen: </strong>{this.state.game.fen()}</p>
+            <p className="console"><strong>msg: </strong>{this.state.msg}</p>
             </div>
             </>
         )
